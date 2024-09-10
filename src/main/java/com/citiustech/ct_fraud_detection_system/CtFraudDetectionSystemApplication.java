831448d17037242363b9fdfed241ca740faeabf4
Sandeep <sandeepm.mice@gmail.com>
2024-09-10 20:39:40 +0530

    v

---
+++ src/main/java/com/citiustech/ct_fraud_detection_system/CtFraudDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.citiustech.ct_fraud_detection_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtFraudDetectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtFraudDetectionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citiustech/ct_fraud_detection_system/CtFraudDetectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citiustech.ct_fraud_detection_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtFraudDetectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
